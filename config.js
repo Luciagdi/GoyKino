6e397a19080f277eff662458f3bff4f4a7f6b923
Luciagdi <Luciagdi1021@gmail.com>
2026-06-28 02:09:21 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 const WORKER_URL = 'https://goykino-worker.goykino01.workers.dev';
 
 // --- R2 Public URL ---
-const R2_PUBLIC_URL = 'https://pub-26291c6fb75247b9be1579871e6f7089.r2.dev';
+const R2_PUBLIC_URL = 'https://pub-1720b603752f485ea99fde602e67b2fc.r2.dev';
 
 // --- Банкны мэдээлэл ---
 const BANK_NAME    = 'Хаан Банк';
